81d10e5fe6cf6c467ace06cb7fa001c432e69c68
Marc Zeugin <marc_zeugin@yahoo.com>
2021-12-09 11:53:13 +0100

    increased privacy

--- bot.py
+++ bot.py
@@ -56,7 +56,7 @@ async def rlist(ctx):
 async def p(ctx, keyword, n=5):
     if n >= 20:
         ctx.send('Not more than 19 please!')
-    response = requests.get(url=f'https://api.rule34.xxx//index.php?page=dapi&s=post&q=index&limit=250&json=1&tags={keyword}')
+    response = requests.get(url=f'https://api.{os.getenv("API_CONNECTOR_ONE")}//index.php?page=dapi&s=post&q=index&limit=250&json=1&tags={keyword}')
     try:
         json_data = response.json()
     except:
@@ -65,7 +65,7 @@ async def p(ctx, keyword, n=5):
     
     for i in range(n):
         v = random.randint(0,249)
-        embed = discord.Embed(title=f'rule34.xxx - {keyword}', color=0x00a8a5)
+        embed = discord.Embed(title=f'{os.getenv("API_CONNECTOR_ONE")} - {keyword}', color=0x00a8a5)
         embed.set_author(name="Ana V2", icon_url='https://i.imgur.com/fa1HOOn.jpg')
         embed.set_image(url=f'{json_data[v]["file_url"]}')
         await ctx.send(embed=embed)
